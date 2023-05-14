31ffc1d6f12786f130a7e8507d64db200a1ac931
dbquiroga <day@xcapit.com>
2023-05-14 17:48:17 -0300

    background color into text titles

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,17 +12,21 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)} style={{flex:1, backgroundImage:"/img/docusaurus.png", backgroundSize:'100%'}}> 
       <div className="container">
-        <div className='avatarArea'>
-         <img src={require("/img/me.png").default}alt="Day Quiroga" style={{ width: "100px", borderRadius: "50%" }}/>
-        </div>
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--md"
-            to="/docs/intro">
-            Ir a la documentación✨
-          </Link>
+        <div className='heroTextContainer'>
+          <div className='avatarArea'>
+          <img src={require("/img/me.png").default}alt="Day Quiroga" style={{ width: "100px", borderRadius: "50%" }}/>
+          </div>
+          <div className='heroTextArea'>
+          <p className="heroTextTitle">{siteConfig.title}</p>
+          <p className="heroTextSubTitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--md"
+              to="/docs/intro">
+              Ir a la documentación✨
+            </Link>
+          </div>
+          </div>
         </div>
       </div>
     </header>
